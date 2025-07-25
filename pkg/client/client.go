dc0f8b6913ee683b68d0b4b122a2758907981197
Steve Gontzes <steve.gontzes@conductorone.com>
2025-07-25 15:57:28 -0400

    Revert "sort /users with service accounts upfront (#17)" (#18)
    This reverts commit 9b9f216079bc6bddd241d264b8561b2e4f44eb4d.

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -56,12 +56,7 @@ func (c *Client) getUsers(ctx context.Context, pageToken *string, includeService
 	// API Doc: https://api.sumologic.com/docs/#operation/listUsers
 	path := "/api/{{.apiVersion}}/users"
 	pathParameters := map[string]string{"apiVersion": apiVersion}
-	queryParams := map[string]string{
-		"includeServiceAccounts": fmt.Sprintf("%t", includeServiceAccounts),
-		// Sort by last name in ascending order.
-		// Service accounts have empty strings, so they should be at the beginning.
-		"sortBy": "lastName",
-	}
+	queryParams := map[string]string{"includeServiceAccounts": fmt.Sprintf("%t", includeServiceAccounts)}
 
 	var response ApiResponse[Account]
 
